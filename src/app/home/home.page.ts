f8ea35c3fd3bc6dca7f875f07bb4a91562bd719e
Marius Bolik <marius@eyloo.com>
2024-03-12 13:38:33 +0100

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
+  providers: [HttpClient]
 })
 export class HomePage {
-  constructor() {}
+
+  public data = signal(null);
+  
+  private httpClient = inject(HttpClient);
+
+  fetchData() {
+    this.httpClient.get('https://cors-test-api.vercel.app/api/cors-test').subscribe((data: any) => {
+      console.log(data);
+      this.data.set(data);
+    });
+  }
 }
